f5928442406e29f62a7dd135d19aa893039c4aec
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-05-07 12:12:38 +0600

    updated

--- src/app/error.js
+++ src/app/error.js
@@ -11,7 +11,7 @@ const ErrorPage = () => {
         router.push('/')
     }
   return (
-    <div className="mt-5">
+    <div className="mt-5 flex justify-center items-center flex-col">
       <Image  className="w-[50vw] h-[40vw]" src={errorImg} alt="Error" width={1000} height={1000}/>
       <button onClick={handleBackBtn} className="btn btn-error">Back To HomePage</button>
     </div>
